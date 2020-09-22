7c8ed710b997e37eb048a207b33bca4320b41f77
Joaquin Barco <joaezeb@gmail.com>
2020-09-22 20:21:05 -0300

    Resuelvo colision

--- prueba.java
+++ prueba.java
@@ -1,9 +1,5 @@
 public class prueba {
 	public static void main(String[] args){
-<<<<<<< HEAD
 		System.out.println("Hola Mundo Peri!");
-=======
-		System.out.println("Hola Mundo Joaquin!");
->>>>>>> bfe12c83667ee7034caa05c88763e8396ac78975
 	}	
 }
